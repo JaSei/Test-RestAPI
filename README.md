fc9726dbe7672de38d6a9ef6c0776ffe550aa69e
Jan Seidl <seidl@avast.com>
2016-10-06 16:27:18 +0200

    scaffold

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::RestAPI - It's new $module
+
+# SYNOPSIS
+
+    use Test::RestAPI;
+
+# DESCRIPTION
+
+Test::RestAPI is ...
+
+# LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jan Seidl <seidl@avast.com>
---
+++ lib/Test/RestAPI.pm
@@ -0,0 +1,39 @@
+package Test::RestAPI;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::RestAPI - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::RestAPI;
+
+=head1 DESCRIPTION
+
+Test::RestAPI is ...
+
+=head1 LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Seidl E<lt>seidl@avast.comE<gt>
+
+=cut
+
